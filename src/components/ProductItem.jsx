57f46eb44251f991428c1c3ab655129d73c42bde
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-05-26 13:08:42 -0300

    homePage styling

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types'
 const ProductItem = ({ product }) => {
 	return (
 		<div key={product.id} className="group relative">
-			<div className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-				<img src={product.image?.url} alt={product.name} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+			<div className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 ">
+				<img src={product.image?.url} alt={product.name} className="object-cover object-center " />
 			</div>
 			<div className="mt-4 flex justify-between">
 				<div>
-					<h3 className="text-sm text-white">
+					<h3 className="text-base text-white">
 						{/* <a href={product.href}> */}
 						<span aria-hidden="true" className="absolute inset-0" />
 						{product.name}
 						{/* </a> */}
 					</h3>
 				</div>
-				<p className="text-sm font-medium text-white">$ {product.price.raw}</p>
+				<p className="text-base font-medium text-white">$ {product.price.raw}</p>
 			</div>
 		</div>
 	)
