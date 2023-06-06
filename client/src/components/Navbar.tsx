c3952419c2f0d5d2dd51c3c1f4c02e4ae71d5816
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-06-06 13:59:19 -0300

    responsiveness issues

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar: React.FC = () => {
 
 	return (
 		<nav className={`sticky top-0 z-50 bg-gray-100 shadow-md transition-all duration-300 ${visible ? 'opacity-100' : 'opacity-0'}`}>
-			<div className="container mx-auto px-6 py-3 text-gray-700 md:flex md:justify-between">
+			<div className="container mx-auto px-6 py-2 text-gray-700 md:flex md:justify-between">
 				<div className="flex items-center justify-between">
 					<a href="#">
 						<img className="h-6 w-auto sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
 					</a>
 
-					<div className="flex lg:hidden">
+					<div className="flex md:hidden">
 						<button
 							onClick={() => setIsOpen(!isOpen)}
 							type="button"
@@ -46,9 +46,9 @@ const Navbar: React.FC = () => {
 				<div
 					className={`${
 						isOpen ? 'translate-x-0  opacity-100' : '-translate-x-full opacity-0'
-					} absolute inset-x-0 z-20 w-full bg-white px-6 py-4 transition-all duration-300 ease-in-out md:relative md:top-0 md:mt-0 md:flex md:translate-x-0 md:items-center md:bg-transparent md:p-0 md:opacity-100`}
+					} absolute inset-x-0 z-20 bg-gray-100 py-4 transition-all duration-300 ease-in-out md:relative md:top-0 md:mt-0 md:flex md:translate-x-0 md:items-center md:bg-transparent md:p-0 md:opacity-100`}
 				>
-					<div className="-mx-4 flex flex-col px-2 md:mx-10 md:ml-auto md:flex-row md:items-center md:p-0 md:opacity-100">
+					<div className="flex flex-col px-2 md:flex-row md:items-center md:p-0 md:opacity-100 lg:mr-20 xl:mr-52 2xl:mr-80">
 						<a href="#" className="transform rounded-lg px-2.5 py-2 transition-colors duration-300 md:mx-2">
 							About
 						</a>
@@ -68,7 +68,7 @@ const Navbar: React.FC = () => {
 
 							<input
 								type="text"
-								className="w-full rounded-lg border bg-transparent py-2 pl-[2.7rem] pr-4 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+								className="rounded-lg border bg-transparent py-2 pl-[2.7rem] text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 md:w-52 lg:w-full"
 								placeholder="Search"
 							/>
 						</div>
