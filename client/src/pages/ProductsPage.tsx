b89f6d6db7416a3845671bc0ab0d08876d513585
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-06-08 13:56:23 -0300

    converted product card into a component

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -1,124 +1,76 @@
-import { MdOutlineAddShoppingCart } from 'react-icons/md'
+import { useState } from 'react'
+import ProductCard from '../components/ProductCard'
 import { HiOutlineAdjustmentsHorizontal } from 'react-icons/hi2'
-import { GrClose } from 'react-icons/gr'
+import { Select, Option } from '@material-tailwind/react'
 
-const products = [
-	{
-		id: 1,
-		name: 'Earthen Bottle',
-		href: '#',
-		price: '$48',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-		imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.'
-	},
-	{
-		id: 2,
-		name: 'Nomad Tumbler',
-		href: '#',
-		price: '$35',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-		imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.'
-	},
-	{
-		id: 3,
-		name: 'Focus Paper Refill',
-		href: '#',
-		price: '$89',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-		imageAlt: 'Person using a pen to cross a task off a productivity paper card.'
-	},
-	{
-		id: 4,
-		name: 'Earthen Bottle',
-		href: '#',
-		price: '$48',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-		imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.'
-	},
-	{
-		id: 5,
-		name: 'Nomad Tumbler',
-		href: '#',
-		price: '$35',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-		imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.'
-	},
-	{
-		id: 6,
-		name: 'Focus Paper Refill',
-		href: '#',
-		price: '$89',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-		imageAlt: 'Person using a pen to cross a task off a productivity paper card.'
-	},
-	{
-		id: 7,
-		name: 'Machined Mechanical Pencil',
-		href: '#',
-		price: '$35',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-		imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.'
+const ProductsPage = () => {
+	const [isOpen, setIsOpen] = useState(false)
+	const [selectedOption, setSelectedOption] = useState('')
+
+	const handleOptionSelect = (option: string) => {
+		setSelectedOption(option)
+		setIsOpen(false)
+		// Add your logic here for sorting based on the selected option
 	}
-	// More products...
-]
 
-const ProductsPage = ({ setIsNavbarVisible }: { setIsNavbarVisible: (isVisible: boolean) => void }) => {
 	return (
-		<div className="relative">
-			<div className="drawer drawer-end">
-				<input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-				<div className="drawer-content">
-					<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
-						<div className="mb-4 flex items-center justify-between">
-							<h2 className="text-2xl font-bold text-gray-700">All Products</h2>
-							<label
-								htmlFor="my-drawer-4"
-								onClick={() => setIsNavbarVisible(false)}
-								className="drawer-button ml-auto flex items-center rounded-full bg-gray-300 px-5 py-3 font-bold"
-							>
-								<span className="mr-2">
-									<HiOutlineAdjustmentsHorizontal size={24} color="black" />
-								</span>
-								Filter &amp; Sort
-							</label>
-						</div>
+		<div className="mx-auto max-w-7xl">
+			<div className="px-4 py-8 sm:px-6 md:py-16 lg:px-8">
+				<div className="mb-4 flex items-center justify-between">
+					<h2 className="text-2xl font-bold text-gray-700">
+						All Products <span className="block pl-1 text-sm font-thin sm:inline">7 products</span>
+					</h2>
 
-						<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
-							{products.map((product) => (
-								<a key={product.id} href={product.href} className="group">
-									<div className="aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 relative w-full overflow-hidden rounded-lg bg-gray-200">
-										<img
-											src={product.imageSrc}
-											alt={product.imageAlt}
-											className="h-full w-full object-cover object-center group-hover:opacity-75"
-										/>
-										<button
-											title="Add to cart"
-											className="absolute bottom-4 left-1/2 flex -translate-x-1/2 transform items-center rounded-full bg-white px-6 py-2 opacity-0 transition-colors duration-300 group-hover:opacity-100"
-										>
-											<MdOutlineAddShoppingCart size={24} color="gray" />
-										</button>
-									</div>
-									<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-									<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-								</a>
-							))}
-						</div>
+					{/* Dropdown Menu */}
+					<div className="relative inline-block">
+						<button className="ml-auto flex items-center rounded-full bg-gray-300 px-5 py-3 font-bold" onClick={() => setIsOpen(!isOpen)}>
+							<span className="mr-2">
+								<HiOutlineAdjustmentsHorizontal size={24} color="black" />
+							</span>
+							Sort by {!selectedOption ? 'Relevancy' : selectedOption}
+						</button>
+						{isOpen && (
+							<ul className="absolute left-0 z-10 mt-2 w-60 rounded-lg border border-gray-300 bg-white shadow-lg">
+								<li className="cursor-pointer px-4 py-2 hover:bg-gray-200" onClick={() => handleOptionSelect('Price: Low to High')}>
+									Price: Low to High
+								</li>
+								<li className="cursor-pointer px-4 py-2 hover:bg-gray-200" onClick={() => handleOptionSelect('Price: High to Low')}>
+									Price: High to Low
+								</li>
+								<li className="cursor-pointer px-4 py-2 hover:bg-gray-200" onClick={() => handleOptionSelect('Relevancy')}>
+									Relevancy
+								</li>
+								<li className="cursor-pointer px-4 py-2 hover:bg-gray-200" onClick={() => handleOptionSelect('Newest')}>
+									Newest
+								</li>
+							</ul>
+						)}
 					</div>
 				</div>
-				<div className="drawer-side">
-					<label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-					<input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-					<div className="menu h-full w-80 bg-base-200 p-4 text-base-content">
-						<GrClose className="cursor-pointer text-2xl text-gray-500" />
-						<ul>
-							<li>
-								<a>Sidebar Item 1</a>
-							</li>
-							<li>
-								<a>Sidebar Item 2</a>
-							</li>
-						</ul>
+
+				{/* Filters */}
+				<div className="flex flex-col sm:flex-row">
+					<div className="w-full space-y-3 pr-4 sm:mr-4 sm:w-1/3 sm:pr-0">
+						<Select variant="standard" label="Availability" className="w-64 pb-2 font-medium text-gray-700 hover:text-gray-900">
+							<Option>In Stock</Option>
+							<Option>Pre-Order</Option>
+						</Select>
+						<Select variant="standard" label="Category" className="w-64 pb-2 font-medium text-gray-700 hover:text-gray-900">
+							<Option>Barbells</Option>
+							<Option>Plates</Option>
+							<Option>Accesories</Option>
+						</Select>
+						<Select variant="standard" label="Collections" className="w-64 font-medium text-gray-700 hover:text-gray-900">
+							<Option>Powerlifting</Option>
+							<Option>Calisthenics</Option>
+							<Option>Functional</Option>
+							<Option>Rig & Rack Equipment</Option>
+						</Select>
+					</div>
+
+					{/* Products List */}
+					<div className="flex flex-grow flex-col">
+						<ProductCard />
 					</div>
 				</div>
 			</div>
@@ -127,35 +79,3 @@ const ProductsPage = ({ setIsNavbarVisible }: { setIsNavbarVisible: (isVisible: 
 }
 
 export default ProductsPage
-
-{
-	/* <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8">
-			<div className="mb-4 flex items-center justify-between">
-				<h2 className="text-2xl font-bold text-gray-700">All Products</h2>
-				<button className="ml-auto flex items-center rounded-full bg-gray-300 px-5 py-3 font-bold">
-					<span className="mr-2">
-						<HiOutlineAdjustmentsHorizontal size={24} color="black" />
-					</span>
-					Filter &amp; Sort
-				</button>
-			</div>
-	
-			<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
-				{products.map((product) => (
-					<a key={product.id} href={product.href} className="group">
-						<div className="aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 relative w-full overflow-hidden rounded-lg bg-gray-200">
-							<img src={product.imageSrc} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75" />
-							<button
-								title="Add to cart"
-								className="absolute bottom-4 left-1/2 flex -translate-x-1/2 transform items-center rounded-full bg-white px-6 py-2 opacity-0 transition-colors duration-300 group-hover:opacity-100"
-							>
-								<MdOutlineAddShoppingCart size={24} color="gray" />
-							</button>
-						</div>
-						<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-						<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-					</a>
-				))}
-			</div>
-		</div> */
-}
